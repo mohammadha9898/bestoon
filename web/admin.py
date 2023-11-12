4efaec298fb963354a76eda3aaf17fcab7953a45
mohammadha98 <mohammadzolfaqar98@gmail.com>
2023-11-12 17:05:08 +0330

    expense model and admin

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
+from os import name
 from django.contrib import admin
+from .models import Expense
 
 # Register your models here.
+class ExpenseAdmin (admin.ModelAdmin):
+    list_display = ("text","user","date")
+
+admin.site.register(Expense, ExpenseAdmin)
