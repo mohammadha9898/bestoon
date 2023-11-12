0cb213c4e447be1f39156a7d73991b35e9b92d09
mohammadha98 <mohammadzolfaqar98@gmail.com>
2023-11-12 17:40:19 +0330

    Income is here

--- web/admin.py
+++ web/admin.py
@@ -1,9 +1,13 @@
 from os import name
 from django.contrib import admin
-from .models import Expense
+from .models import Expense , Income
 
 # Register your models here.
 class ExpenseAdmin (admin.ModelAdmin):
     list_display = ("text","user","date")
 
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ("text","user","date")
+
 admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Income , IncomeAdmin)
--- web/models.py
+++ web/models.py
@@ -6,3 +6,8 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User , on_delete= models.PROTECT)
 
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete = models.PROTECT)
