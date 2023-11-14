1e10900fad53b5ee1b9b2fa2bdeeec49ac41888f
mohammadha98 <mohammadzolfaqar98@gmail.com>
2023-11-14 14:32:57 +0330

    view changed for post requests

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,6 @@
 from os import name
 from django.contrib import admin
-from .models import Expense , Income
+from .models import Expense , Income , Token
 
 # Register your models here.
 class ExpenseAdmin (admin.ModelAdmin):
@@ -8,6 +8,10 @@ class ExpenseAdmin (admin.ModelAdmin):
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ("text","user","date")
+    
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ("user","id")
 
-admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Expense , ExpenseAdmin)
 admin.site.register(Income , IncomeAdmin)
+admin.site.register(Token , TokenAdmin)
--- web/models.py
+++ web/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Token(models.Model):
+    user = models.OneToOneField(User,on_delete=models.PROTECT)
+    token = models.CharField(max_length=48)
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
