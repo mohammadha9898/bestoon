dc95898314f80263842e9c79e6af6d0e5c9771eb
mohammadha98 <mohammadzolfaqar98@gmail.com>
2023-11-19 23:25:16 +0330

    css and others

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,15 @@ from . import views
 
 urlpatterns = [
     path('submit/expense/', views.submit_expense,name='submit_expense'),
-    path('submit/income/', views.submit_income,name='submit_income')
+    path('submit/income/', views.submit_income,name='submit_income'),
+    path('accounts/register/', views.register,name='register'),
+    path('', views.index, name='index'),
+    path('edit/expense/',views.edit_expense, name='edit_expense'),
+    path('edit/income/',views.edit_income, name ='edit_income'),
+    path('q/incomes/',views.query_incomes, name='query_incomes'),
+    path('q/expenses/',views.query_expenses, name='query_expenses'),
+    path('accounts/whoami/', views.whoami, name='whoami'),
+    path('accounts/login/',views.login, name='login'),
+    path('news/', views.news, name='news'),
+    path('accounts/logout/',views.logout, name='logout'),
 ]
-
